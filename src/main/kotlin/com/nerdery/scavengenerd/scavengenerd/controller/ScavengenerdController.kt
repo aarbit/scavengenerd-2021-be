454ab7193cbed715b9ad7e1869ee295aa0514d2e
Alex Arbit <aarbit@gmail.com>
2022-07-11 11:18:38 -0500

    Adds utility functions

--- src/main/kotlin/com/nerdery/scavengenerd/scavengenerd/controller/ScavengenerdController.kt
+++ src/main/kotlin/com/nerdery/scavengenerd/scavengenerd/controller/ScavengenerdController.kt
@@ -5,12 +5,17 @@ import com.nerdery.scavengenerd.scavengenerd.model.*
 import com.nerdery.scavengenerd.scavengenerd.service.EntryService
 import com.nerdery.scavengenerd.scavengenerd.service.ItemService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
 class ScavengenerdController @Autowired constructor(val itemService: ItemService,
                                                     val entryService: EntryService){
+
+    @Value("\${versionNumber}")
+    lateinit var versionNum: String
+
     @GetMapping("items")
     fun getItems(): List<ItemOverview> {
         return itemService.getItems()
@@ -40,5 +45,26 @@ class ScavengenerdController @Autowired constructor(val itemService: ItemService
     @DeleteMapping("entry/{entryId}")
     fun deleteEntry(@PathVariable("entryId") entryId: Long) {
         return entryService.deleteEntry(entryId)
+    }
+
+    @GetMapping("popstat")
+    fun populateCumulativeStatuses() {
+        val items = itemService.itemRepository.findAll()
+        items.map { item ->
+            val entries = entryService.entryRepository.findByItem(item.id!!)
+            val statusSet = entries.toList().map { it.status }.toSet()
+            val cumulativeStatus = when {
+                statusSet.contains(StatusEnum.APPROVED) -> StatusEnum.APPROVED
+                statusSet.contains(StatusEnum.SUBMITTED) -> StatusEnum.SUBMITTED
+                statusSet.contains(StatusEnum.FOUND) -> StatusEnum.FOUND
+                else -> StatusEnum.NOT_FOUND
+            }
+            val updatedItem = item.copy(cumulativeStatus = cumulativeStatus)
+            itemService.itemRepository.save(updatedItem)
+        }
+    }
+    @GetMapping()
+    fun version(): String {
+        return "ScavengeNerd API Version: $versionNum"
     }
 }
